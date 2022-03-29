7e80c42c9f4e5a53be55ee3e1c541b185e3f771b
Nishant <ns114046@gmail.com>
2022-03-29 11:45:13 +0530

    3rd

--- 1st.c
+++ 1st.c
@@ -2,5 +2,13 @@
 #include<stdio.h>
 void main()
 {
+    // variables
+    int j;   //interger value
+    // case sesntive 
+    // some importent rules we have to follow to make any variable name
+
     printf("hello world");
-}
+} 
+
+
+// keywords- some words that was inbulid reserverd there are like 32 keyword in c we can make virable name like keywords like printf scacf
